d43dc43312b1199bb2d73b8e7978340df3f75afa
huhongbo <huhongbo@gmail.com>
2013-08-08 16:42:09 +0800

    change project dntmon format

--- config/projects/dntmon.rb
+++ config/projects/dntmon.rb
@@ -16,68 +16,70 @@
 #
 
 name "dntmon"
+maintainer "Dnt, Inc."
+homepage "http://www.dnt.com.cn"
 
-replaces        "dntmon"
+replaces        "chef-server"
 install_path    "/opt/chef-server"
 build_version   Omnibus::BuildVersion.new.semver
-build_iteration "1"
+build_iteration 1
 
-deps = []
+dependency "preparation"
 
 # global
-deps << "chef-gem" # for embedded chef-solo
-deps << "preparation" # creates required build directories
-deps << "chef-server-cookbooks" # used by chef-server-ctl reconfigure
-deps << "chef-server-scripts" # assorted scripts used by installed instance
-deps << "chef-server-ctl" # additional project-specific chef-server-ctl subcommands
-deps << "nginx" # load balacning
-deps << "runit"
-deps << "unicorn" # serves up Rack apps (chef-server-webui)
+dependency "chef-gem" # for embedded chef-solo
+dependency "chef-server-cookbooks" # used by chef-server-ctl reconfigure
+dependency "chef-server-scripts" # assorted scripts used by installed instance
+dependency "chef-server-ctl" # additional project-specific chef-server-ctl subcommands
+dependency "nginx" # load balacning
+dependency "runit"
+dependency "unicorn" # serves up Rack apps (chef-server-webui)
 
 # the backend
-deps << "postgresql"
-deps << "rabbitmq"
-deps << "chef-solr"
-deps << "chef-expander"
-deps << "bookshelf" # S3 API compatible object store
+dependency "postgresql"
+dependency "rabbitmq"
+dependency "chef-solr"
+dependency "chef-expander"
+dependency "bookshelf" # S3 API compatible object store
 
 # the front-end services
-deps << "erchef" # the actual Chef Server REST API
-deps << "chef-server-webui"
+dependency "erchef" # the actual Chef Server REST API
+dependency "chef-server-webui"
 
 #sensu services
-deps << "redis"
-deps << "sensu"
-deps << "sensu-plugin"
-deps << "sensu-dashboard"
+dependency "redis"
+dependency "sensu"
+dependency "sensu-plugin"
+dependency "sensu-dashboard"
 
 #graylog2 services
-deps << "elasticsearch"
-deps << "graylog2"
-deps << "graylog2-webui"
-deps << "mongodb"
-deps << "logstash"
+dependency "elasticsearch"
+dependency "graylog2"
+dependency "graylog2-webui"
+dependency "mongodb"
+dependency "logstash"
 
 #mcollective services
-deps << "stomp"
-deps << "mcollective"
+dependency "stomp"
+dependency "mcollective"
 
 #other
-deps << "gdash"
-deps << "geminabox"
-deps << "info-dashboard"
+dependency "gdash"
+dependency "geminabox"
+dependency "info-dashboard"
 
 #python
-deps << "python"
-deps << "pip"
+dependency "python"
+dependency "pip"
 
 # integration testing
-deps << "chef-pedant" # test ALL THE THINGS!
+dependency "chef-pedant" # test ALL THE THINGS!
 
 # version manifest file
-deps << "version-manifest"
-
-dependencies deps
+dependency "version-manifest"
 
 exclude "\.git*"
 exclude "bundler\/git"
+
+
+
