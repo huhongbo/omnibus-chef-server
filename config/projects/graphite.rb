edff0d91f1123ef72f9de2f481a00f8c0f2fb583
huhongbo <huhongbo@gmail.com>
2013-08-14 12:13:36 +0800

    add graphite test

---
+++ config/projects/graphite.rb
@@ -0,0 +1,51 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "graphite"
+maintainer "Dnt, Inc."
+homepage "http://www.dnt.com.cn"
+
+replaces        "graphite"
+install_path    "/opt/chef-server"
+build_version   Omnibus::BuildVersion.new.semver
+build_iteration 1
+
+dependency "preparation"
+
+# global
+dependency "chef-gem" # for embedded chef-solo
+dependency "chef-server-cookbooks" # used by chef-server-ctl reconfigure
+dependency "chef-server-scripts" # assorted scripts used by installed instance
+dependency "chef-server-ctl" # additional project-specific chef-server-ctl subcommands
+dependency "nginx" # load balacning
+dependency "runit"
+
+#python
+dependency "python"
+dependency "graphite"
+
+# integration testing
+dependency "chef-pedant" # test ALL THE THINGS!
+
+# version manifest file
+dependency "version-manifest"
+
+exclude "\.git*"
+exclude "bundler\/git"
+
+
+
